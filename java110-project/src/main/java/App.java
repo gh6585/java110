883dfdaa3e982e07f0c0d06882625c8390d832fb
g <rhkdgh0418@daum.net>
2018-09-04 14:37:59 +0900

    배열을 이용하여 여러 회원의 정보저장

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -5,6 +5,13 @@ public class App {
     
 
     public static void main(String[] args) {
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] passwords =new String[100];
+        
+        int index =0;
+        
+        
        //1) 키보드 입력을 처리할 객체 준비
        Scanner keyin= new Scanner(System.in);
        
@@ -12,23 +19,28 @@ public class App {
        
        while (true) {
        System.out.print("이름?");
-       String name = keyin.nextLine();//nextLine 줄단위로 입력받아라 
+       names[index] = keyin.nextLine();//nextLine 줄단위로 입력받아라 
        //블럭킹 ? 입력받을때까지 기다렸다가 리턴
        
        System.out.print("이메일?");
-       String email = keyin.nextLine();
+       emails[index] = keyin.nextLine();
        
        System.out.print("암호?");
-       String password = keyin.nextLine();
+       passwords[index] = keyin.nextLine();
        
-       System.out.printf("%s ,%s, %s\n",name,email,password);
+       index++;
        
        System.out.print("계속하시겠습니까?(y/n)");
        String answer = keyin.nextLine();
        if(answer.toLowerCase().equals("n"))//toLower 메소드 호출후 retrun 값으로 eqpuals
        break;
        
-       
        } 
+       
+       for(int i=0; i< index;i++) {
+       System.out.printf("%s ,%s, %s\n",names[i],emails[i],passwords[i]);
        }
+       
+       keyin.close();
+    }
 }
