4b732d2cfd816f6f4535d9caa6c0d6cf91fb4761
g <rhkdgh0418@daum.net>
2018-09-13 14:17:33 +0900

    data.sql작성

---
+++ java110-db/project-model/data.sql
@@ -0,0 +1,21 @@
+--회원 정보
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (1, 'user01', 'user01@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (2, 'user02', 'user02@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (3, 'user03', 'user03@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (4, 'user04', 'user04@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (5, 'user05', 'user05@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (6, 'user06', 'user06@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (7, 'user07', 'user07@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (8, 'user08', 'user08@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (9, 'user09', 'user09@test.com', password('1111'), now());
+insert into p1_memb(mno,name,email,pwd,cdt)
+value (10, 'user10', 'user10@test.com', password('1111'), now());
