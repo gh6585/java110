f5c6b38f65721fd020062742330ff9891c174e53
g <rhkdgh0418@daum.net>
2018-09-14 17:23:25 +0900

    xxxx/list 요청 처리

--- java110-project/src/main/java/bitcamp/java110/cms/client/ClientApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/client/ClientApp.java
@@ -7,47 +7,42 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientApp {
-
-
+    
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-
+        
         try (
-                //서버에 연결하기
-                Socket socket = new Socket("localhost",8888);
-
-                // 서버에 데이터를 보내고 읽을 도구를 준비하기
-                PrintStream out = new PrintStream(
-                        new BufferedOutputStream(
-                                socket.getOutputStream()));
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(
-                                socket.getInputStream()));
-                ) {
-            out.println("Hello"); out.flush();
+            // 서버에 연결하기
+            Socket socket = new Socket("localhost", 8888);
+            
+            // 서버에 데이터를 보내고 읽을 도구를 준비하기
+            PrintStream out = new PrintStream(
+                                new BufferedOutputStream(
+                                        socket.getOutputStream()));
+            BufferedReader in = new BufferedReader(
+                               new InputStreamReader(
+                                  socket.getInputStream()));
+        ) {
+            out.println("HELLO"); out.flush();
             System.out.println(in.readLine());
-
+            
             while (true) {
                 String requestLine = prompt();
                 out.println(requestLine); out.flush();
-                while(true) {
+                while (true) {
                     String responseLine = in.readLine();
                     System.out.println(responseLine);
-                    if(responseLine.length() == 0)
+                    if (responseLine.length() == 0)
                         break;
                 }
                 if (requestLine.equals("EXIT")){
-                    System.out.println("안녕히 가세요!");
                     break;
                 } 
-
-
             }
-
+            
         }
-
-
+        
         keyIn.close();
     }
 
@@ -56,25 +51,3 @@ public class ClientApp {
         return keyIn.nextLine();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
