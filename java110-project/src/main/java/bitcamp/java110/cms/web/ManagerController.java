75fd7470e011202705e9d882180c0ea5f85ce437
g <rhkdgh0418@daum.net>
2018-11-05 15:21:06 +0900

    ,

--- java110-project/src/main/java/bitcamp/java110/cms/web/ManagerController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/web/ManagerController.java
@@ -20,71 +20,71 @@ import bitcamp.java110.cms.service.ManagerService;
 @Controller
 @RequestMapping("/manager")
 public class ManagerController { 
-    
-    ManagerService managerService;
-    ServletContext sc;
-    
-    public ManagerController(
-            ManagerService managerService,
-            ServletContext sc) {
-        this.managerService = managerService;
-        this.sc = sc;
+
+  ManagerService managerService;
+  ServletContext sc;
+
+  public ManagerController(
+      ManagerService managerService,
+      ServletContext sc) {
+    this.managerService = managerService;
+    this.sc = sc;
+  }
+
+  @GetMapping("list")
+  public void list(
+      @RequestParam(defaultValue="1") int pageNo,
+      @RequestParam(defaultValue="3") int pageSize,
+      Model model) {
+
+    if (pageNo < 1)
+      pageNo = 1;
+
+    if (pageSize < 3 || pageSize > 10)
+      pageSize = 3;
+
+    List<Manager> list = managerService.list(pageNo, pageSize);
+    model.addAttribute("list", list);
+  }
+
+  @GetMapping("detail")
+  public void detail(
+      int no,
+      Model model) {
+
+    Manager m = managerService.get(no);
+    model.addAttribute("manager", m);
+  }
+
+  @GetMapping("form")
+  public void form() {
+
+  }
+
+  @PostMapping("add")
+  public String add(
+      Manager manager,
+      MultipartFile file1) throws Exception {
+
+    // 사진 데이터 처리
+    if (file1.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      file1.transferTo(new File(sc.getRealPath("/upload/" + filename)));
+      manager.setPhoto(filename);
     }
 
-    @GetMapping("list")
-    public void list(
-            @RequestParam(defaultValue="1") int pageNo,
-            @RequestParam(defaultValue="3") int pageSize,
-            Model model) {
-        
-        if (pageNo < 1)
-            pageNo = 1;
-        
-        if (pageSize < 3 || pageSize > 10)
-            pageSize = 3;
-        
-        List<Manager> list = managerService.list(pageNo, pageSize);
-        model.addAttribute("list", list);
-    }
-    
-    @GetMapping("detail")
-    public void detail(
-            int no,
-            Model model) {
-        
-        Manager m = managerService.get(no);
-        model.addAttribute("manager", m);
-    }
-    
-    @GetMapping("form")
-    public void form() {
-        
-    }
-    
-    @PostMapping("add")
-    public String add(
-            Manager manager,
-            MultipartFile file1) throws Exception {
-        
-        // 사진 데이터 처리
-        if (file1.getSize() > 0) {
-            String filename = UUID.randomUUID().toString();
-            file1.transferTo(new File(sc.getRealPath("/upload/" + filename)));
-            manager.setPhoto(filename);
-        }
-        
-        managerService.add(manager);
-        
-        return "redirect:list";
-    }
-    
-    @GetMapping("delete")
-    public String delete(int no) throws Exception {
-        
-        managerService.delete(no);
-        return "redirect:list";
-    }
-    
+    managerService.add(manager);
+
+    return "redirect:list";
+  }
+
+  @GetMapping("delete")
+  public String delete(int no) throws Exception {
+
+    managerService.delete(no);
+    return "redirect:list";
+  }
+
 }
 
 
--- java110-project/src/main/java/bitcamp/java110/cms/web/StudentController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/web/StudentController.java
@@ -20,65 +20,65 @@ import bitcamp.java110.cms.service.StudentService;
 @Controller
 @RequestMapping("/student")
 public class StudentController { 
-    
-    StudentService studentService;
-    ServletContext sc;
-    
-    public StudentController(StudentService studentService, ServletContext sc) {
-        super();
-        this.studentService = studentService;
-        this.sc = sc;
+
+  StudentService studentService;
+  ServletContext sc;
+
+  public StudentController(StudentService studentService, ServletContext sc) {
+    super();
+    this.studentService = studentService;
+    this.sc = sc;
+  }
+
+  @GetMapping("list")
+  public void list(
+      @RequestParam(defaultValue="1") int pageNo,
+      @RequestParam(defaultValue="3") int pageSize,
+      Model model) {
+    if (pageNo < 1)
+      pageNo = 1;
+
+    if (pageSize < 3 || pageSize > 10)
+      pageSize = 3;
+
+    List<Student> list = studentService.list(pageNo, pageSize);
+    model.addAttribute("list", list);
+  }
+
+  @GetMapping("detail")
+  public void detail(
+      int no,
+      Model model) {
+
+    Student s = studentService.get(no);
+    model.addAttribute("student", s);
+  }
+
+  @GetMapping("form")
+  public void form() {
+
+  }
+  @PostMapping("add")
+  public String add(
+      Student student,
+      MultipartFile file1) throws Exception {
+
+    if (file1.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      file1.transferTo(new File(sc.getRealPath("/upload/" + filename)));
+      student.setPhoto(filename);
     }
 
-    @GetMapping("list")
-    public void list(
-            @RequestParam(defaultValue="1") int pageNo,
-            @RequestParam(defaultValue="3") int pageSize,
-            Model model) {
-        if (pageNo < 1)
-            pageNo = 1;
+    studentService.add(student);
 
-        if (pageSize < 3 || pageSize > 10)
-            pageSize = 3;
-        
-        List<Student> list = studentService.list(pageNo, pageSize);
-        model.addAttribute("list", list);
-    }
-    
-    @GetMapping("detail")
-    public void detail(
-            int no,
-            Model model) {
+    return "redirect:list";
 
-        Student s = studentService.get(no);
-        model.addAttribute("student", s);
-    }
-    
-    @GetMapping("form")
-    public void form() {
-        
-    }
-    @PostMapping("add")
-    public String add(
-            Student student,
-            MultipartFile file1) throws Exception {
-        
-        if (file1.getSize() > 0) {
-            String filename = UUID.randomUUID().toString();
-            file1.transferTo(new File(sc.getRealPath("/upload/" + filename)));
-            student.setPhoto(filename);
-        }
-        
-        studentService.add(student);
-        
-        return "redirect:list";
-        
-    }
-    
-    @GetMapping("delete")
-    public String delete(int no) throws Exception {
-        
-        studentService.delete(no);
-        return "redirect:list";
-    }
+  }
+
+  @GetMapping("delete")
+  public String delete(int no) throws Exception {
+
+    studentService.delete(no);
+    return "redirect:list";
+  }
 }
